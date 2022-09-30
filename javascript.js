12e04048b50d875d8d5131e5f4af33f691f17013
Jake Ashton <jakemaddenashton@gmail.com>
2022-09-30 15:36:12 -0700

    Set up basic form validation and custom error messages

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
-let library = [];
-const form = document.querySelector('form');
+const library = [];
+const form = document.querySelector("form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const haveRead = document.querySelector("input:checked");
+const radio = document.querySelector("fieldset");
+const fields = [title, author, pages, radio];
+const fieldError = document.querySelectorAll(".field-error");
 
 class Book {
   constructor(title, author, pages, haveRead) {
@@ -10,90 +17,117 @@ class Book {
   }
 
   readStatus() {
-    if (this.haveRead.toLowerCase() === 'yes') {
-      this.haveRead = 'no';
-    }
-    else this.haveRead = 'yes';
+    if (this.haveRead.toLowerCase() === "yes") {
+      this.haveRead = "no";
+    } else this.haveRead = "yes";
 
     return this.haveRead;
   }
-};
+}
 
-form.addEventListener('submit', addBookToLibrary);
- 
+form.addEventListener("submit", () => {
+  if (validateForm() === true) {
+    addBookToLibrary();
+  }
+});
+
+fields.forEach((field) => {
+  field.addEventListener("input", () => {
+    const index = field.dataset.number;
+    fieldError[index].textContent = "";
+  });
+});
 
 function addBookToLibrary() {
-
   // Set book properties from user form
-  let book = new Book;
-  book.title = document.getElementById('title').value;
-  book.author = document.getElementById('author').value;
-  book.pages = document.getElementById('pages').value;
-  book.haveRead = document.querySelector("input:checked").value;
+  const book = new Book();
+  book.title = title.value;
+  book.author = author.value;
+  book.pages = pages.value;
+  book.haveRead = haveRead.value;
 
   // Add book to library, reset/hide form, add book to page
   library.push(book);
   form.reset();
-  form.setAttribute('hidden', '');
+  form.setAttribute("hidden", "");
   displayBook();
 }
 
-function displayBook() {
+function validateForm() {
+  fields.forEach((field) => {
+    if (!field.validity.valid || haveRead === null) {
+      showError(field);
+      return false;
+    } else return true;
+  });
+}
 
-  let currentBookNumber = library.length;
-  let currentBook = library[currentBookNumber - 1];
+function showError(field) {
+  console.log(field);
+  console.log(haveRead);
+  const index = field.dataset.number;
+  if (field.validity.valueMissing) {
+    fieldError[index].textContent = `Ya gotta enter the ${field.id} ya bungus!`;
+  } else if (
+    field.id === "author" &&
+    field.length < 3 &&
+    /\s/.test(field) === false
+  ) {
+    fieldError[index].textContent = "Gimme a real author you son of a bitch!";
+  } else if (field.id === "read") {
+    console.log(index);
+    fieldError[index].textContent = "Well?";
+  }
+}
+
+function displayBook() {
+  const currentBookNumber = library.length;
+  const currentBook = library[currentBookNumber - 1];
 
   // Add row to table
-  const table = document.querySelector('tbody');
-  let row = document.createElement('tr');
-  row.classList.add('book')
+  const table = document.querySelector("tbody");
+  const row = document.createElement("tr");
+  row.classList.add("book");
   table.append(row);
 
   // Add book data to row
-  let currentRow = table.lastChild;
+  const currentRow = table.lastChild;
   removeBook(currentRow);
   for (const property in currentBook) {
-    if (property == 'haveRead') {
-      let data = document.createElement('td');
+    if (property === "haveRead") {
+      const data = document.createElement("td");
       currentRow.append(data);
-      let read = document.createElement('button');
+      const read = document.createElement("button");
       read.textContent = `${currentBook[property]}`;
       data.append(read);
-      read.addEventListener('click', () => {
+      read.addEventListener("click", () => {
         read.textContent = currentBook.readStatus();
       });
-    }
-    else if (currentBook.hasOwnProperty(property)) {
-      let data = document.createElement('td');
-      data.textContent = `${currentBook[property]}`
+    } else if (currentBook.hasOwnProperty(property)) {
+      const data = document.createElement("td");
+      data.textContent = `${currentBook[property]}`;
       currentRow.append(data);
     }
   }
 }
 
 function removeBook(currentRow) {
-
   // Add remove button
-  let data = document.createElement('td');
+  const data = document.createElement("td");
   currentRow.append(data);
-  let remove = document.createElement('button');
-  remove.textContent = '-';
-  remove.classList.add('remove');
+  let remove = document.createElement("button");
+  remove.textContent = "-";
+  remove.classList.add("remove");
   data.append(remove);
 
   // Add remove functionality
   remove = currentRow.firstChild;
-  remove.addEventListener('click', () => 
-    currentRow.remove())
+  remove.addEventListener("click", () => currentRow.remove());
 }
 
-let newBook = document.querySelector('button.new');
-newBook.addEventListener('click', () => {
-  if (form['hidden']) {
-    form.removeAttribute('hidden')
-  }
-  else form.setAttribute('hidden', '')
+const newBook = document.querySelector("button.new");
+newBook.addEventListener("click", () => {
+  if (form.hidden) {
+    form.removeAttribute("hidden");
+  } else form.setAttribute("hidden", "");
 });
-
-
-
